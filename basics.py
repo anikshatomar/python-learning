338af369c37fcbbbdca65118d65df8d72f8964cd
devangtomar <devangtomar123@gmail.com>
2023-12-28 00:16:52 +0530

    Adding basics of Python

---
+++ basics.py
@@ -0,0 +1,50 @@
+# Learning how print works
+
+print(34+45)
+print(34*45)
+print("Hey Aniksha!")
+print(99+ 83/23*42)
+print("Devang "*5)
+
+# Learning how variables works
+
+num_1 = 45
+num_2 = 2
+
+print(45 * 2)
+print(num_1 * num_2)
+
+# How to take input from user
+
+num_1 = input("Enter the first number!")
+num_2 = input("Enter the second number!")
+
+
+print("Multiplication of above 2 numbers : ")
+print(num_1 * num_2)
+
+name = input("Enter your name")
+print("your name is :")
+print(name)
+
+# How list works
+
+numbers = [1,2,3,4]
+fruits = ["banana", "orange", "apple", "kiwi"]
+
+print(numbers)
+print(fruits)
+
+# Conditional statement :
+# 1. if, elif, else
+# 2. switch case
+
+age = int(input("Enter your age?"))
+
+if age < 18 :
+    print("No! You are not allowed to visit this website!")
+elif age < 30 and age > 18:
+    print("Yes! But be careful!")
+else:
+    print("Yes! You are allowed to visit this website!")
+
