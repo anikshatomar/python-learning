0d5e2b201cc9dfcb3ee9433e27e8157b13ea015e
anikshatomar <aniksha.tomar.22@gmail.com>
2024-01-04 01:18:50 +0530

    strings & arithmetic operations &

--- basics/basics.py
+++ basics/basics.py
@@ -1,50 +1,72 @@
 # Learning how print works
 
-print(34+45)
-print(34*45)
-print("Hey Aniksha!")
-print(99+ 83/23*42)
-print("Devang "*5)
+# print(34+45)
+# print(34*45)
+# print("Hey Aniksha!")
+# print(99+ 83/23*42)
+# print("Devang "*5)
 
-# Learning how variables works
+# # Learning how variables works
 
-num_1 = 45
-num_2 = 2
+# num_1 = 45
+# num_2 = 2
 
-print(45 * 2)
-print(num_1 * num_2)
+# print(45 * 2)
+# print(num_1 * num_2)
 
-# How to take input from user
+# # How to take input from user
 
-num_1 = input("Enter the first number!")
-num_2 = input("Enter the second number!")
+# num_1 = input("Enter the first number!")
+# num_2 = input("Enter the second number!")
 
 
-print("Multiplication of above 2 numbers : ")
-print(num_1 * num_2)
+# print("Multiplication of above 2 numbers : ")
+# print(num_1 * num_2)
 
-name = input("Enter your name")
-print("your name is :")
-print(name)
+# name = input("Enter your name")
+# print("your name is :")
+# print(name)
 
-# How list works
+# # How list works
 
-numbers = [1,2,3,4]
-fruits = ["banana", "orange", "apple", "kiwi"]
+# numbers = [1,2,3,4]
+# fruits = ["banana", "orange", "apple", "kiwi"]
 
-print(numbers)
-print(fruits)
+# print(numbers)
+# print(fruits)
 
-# Conditional statement :
-# 1. if, elif, else
-# 2. switch case
+# # Conditional statement :
+# # 1. if, elif, else
+# # 2. switch case
 
-age = int(input("Enter your age?"))
+# age = int(input("Enter your age?"))
 
-if age < 18 :
-    print("No! You are not allowed to visit this website!")
-elif age < 30 and age > 18:
-    print("Yes! But be careful!")
-else:
-    print("Yes! You are allowed to visit this website!")
+# if age < 18 :
+#     print("No! You are not allowed to visit this website!")
+# elif age < 30 and age > 18:
+#     print("Yes! But be careful!")
+# else:
+#     print("Yes! You are allowed to visit this website!")
+
+#strings:
+
+#name = "aniksha"
+
+#print(name.upper())
+#print(name)
+#print(name.lower()) 
+#print(name. find('h'))
+#print(name.replace("aniksha", "kajal chaudhary"))
+#print(name)
+#print(name.replace('a', 't'))
+#print('k' in name
+#print('o' in name)
+
+#Arithmetic operators:
+#print(5**2)
+#print(5%2)
+ 
+#for ex- 
+i = 5
+print(i + 5)
 
---
+++ conditional-statements/exercise.py
@@ -0,0 +1,9 @@
+#name = "Tony Shark"
+#age =  51
+#print(name)
+#print(age)
+#print("he is genius")
+
+
+name = input("what is your name?")
+print("Hello" + name)
